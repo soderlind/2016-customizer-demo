9877fa8a80fdf2d8655ebf6befa9d5f67ebf0224
soderlind <per@soderlind.no>
2016-12-07 10:25:04 +0100

    Add toggle control, beta code

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
  * @version 1.0.0
  */
 function theme_enqueue_styles() {
-	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', array(), rand() );
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 
@@ -19,6 +19,7 @@ add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
  * @param   WP_Customize_Manager    $wp_customize
  */
 function customize_post_meta( $wp_customize ) {
+
 	$wp_customize->add_section( 'post_meta', array(
 			'title'    => _x( 'Post Meta', 'customizer menu section', '2016-customizer-demo' ),
 			'priority' => 25,
@@ -27,6 +28,7 @@ function customize_post_meta( $wp_customize ) {
 			'default'    => '1',
 			'capability' => 'manage_options',
 			'transport' => 'postMessage',
+			// 'transport' => 'refresh',
 	) );
 	$wp_customize->add_control( 'show_author', array(
 			'settings' => 'show_author',
@@ -34,6 +36,16 @@ function customize_post_meta( $wp_customize ) {
 			'section'  => 'post_meta',
 			'type'     => 'checkbox',
 	) );
+
+	// $wp_customize->selective_refresh->add_partial( 'show_author', array(
+	//     'selector' => '.byline',
+	//     'container_inclusive' => false,
+	//     // 'render_callback' => function() {
+	// 	// 	echo  ( '1' === get_theme_mod( 'show_author' ) ) ? 0: 1;
+	//     //     // bloginfo( 'description' );
+	//     // },
+	// ) );
+
 	$wp_customize->add_setting( 'show_date', array(
 			'default'    => '1',
 			'capability' => 'manage_options',
@@ -46,18 +58,27 @@ function customize_post_meta( $wp_customize ) {
 			'type'     => 'checkbox',
 	) );
 
+	// $wp_customize->selective_refresh->add_partial( 'show_date', array(
+	//     'selector' => '.posted-on a',
+	//     'container_inclusive' => false,
+	//     'render_callback' => function() {
+	// 		echo date( get_option( 'date_format' ) );
+	//         // bloginfo( 'description' );
+	//     },
+	// ) );
+
 	$wp_customize->add_section( 'svg_logo', array(
 			'title'    => _x( 'SVG Logo', 'customizer menu section', '2016-customizer-demo' ),
 			'priority' => 10,
 	) );
 
 	$wp_customize->add_setting( 'svg_logo_url', array(
-			'default'       => '5',
+			// 'default'       => '5',
 			'capability'    => 'edit_theme_options',
 			'transport'     => 'postMessage',
 	) );
 
-	$wp_customize->add_control( new DSS_SVG_Picker_Option ( $wp_customize, 'svg_logo_url', array(
+	$wp_customize->add_control( new DSS_SVG_Picker_Option( $wp_customize, 'svg_logo_url', array(
 			// 'label'	      => esc_html__( 'Select SVG Logo', '2016-customizer-demo' ),
 			'section'     => 'svg_logo',
 			'settings'    => 'svg_logo_url',
@@ -65,7 +86,7 @@ function customize_post_meta( $wp_customize ) {
 	) ) );
 
 	$wp_customize->add_setting( 'svg_logo_width', array(
-			'default'       => '240',
+			'default'       => get_theme_mod( 'svg_logo_width', '240' ),
 			'capability'    => 'edit_theme_options',
 			'transport'     => 'postMessage',
 	) );
@@ -83,6 +104,54 @@ function customize_post_meta( $wp_customize ) {
 		  ),
 	) );
 
+	// $wp_customize->add_setting( 'svg_logo_remove', array(
+	// 		'default'       => true,
+	// 		'capability'    => 'edit_theme_options',
+	// 		'transport'     => 'postMessage',
+	// ) );
+	//
+	// $wp_customize->add_control( 'svg_logo_remove', array(
+	// 		'label'	      => esc_html__( 'Display SVG Logo', '2016-customizer-demo' ),
+	// 		'section'     => 'svg_logo',
+	// 		'settings'    => 'svg_logo_remove',
+	// 		'type'        => 'checkbox',
+	// ) ) ;
+
+	$wp_customize->add_setting( 'svg_logo_remove', array(
+			'default'       => true,
+			'capability'    => 'edit_theme_options',
+			'transport'     => 'postMessage',
+	) );
+
+	$wp_customize->add_control( new DSS_Toggle( $wp_customize, 'svg_logo_remove', array(
+			'label'	      => esc_html__( 'Display SVG Logo', '2016-customizer-demo' ),
+			'section'     => 'svg_logo',
+			'settings'    => 'svg_logo_remove',
+			'type'        => 'toggle',
+			'input_attrs' => array(
+					'off' => 'off text',
+					'on'  => 'on text',
+			),
+	) ) );
+
+	// $wp_customize->selective_refresh->add_partial( 'svg_logo_url', array(
+	//     'selector' => '.svg-logo',
+	//     'container_inclusive' => false,
+	//     'render_callback' => function() {
+	// 		echo date( get_option( 'date_format' ) );
+	//         // bloginfo( 'description' );
+	//     },
+	// ) );
+	//
+	// $partial = $wp_customize->selective_refresh->get_partial( 'custom_logo' );
+	//
+	//
+	// // @codingStandardsIgnoreStart
+	// printf( '<pre>%s</pre>', print_r( $partial, true ) );
+	// // @codingStandardsIgnoreEnd
+	//
+	// $wp_customize->selective_refresh->remove_partial( 'custom_logo');
+
 }
 add_action( 'customize_register','customize_post_meta' );
 
@@ -93,7 +162,7 @@ add_action( 'customize_register','customize_post_meta' );
  * @version 1.0.0
  */
 function customize_preview_post_meta() {
-	wp_enqueue_script( 'customize_preview_post_meta', get_stylesheet_directory_uri() . '/js/customizer.js', array( 'customize-preview', 'jquery' ), rand(), true );
+	wp_enqueue_script( 'customize_preview_post_meta', get_stylesheet_directory_uri() . '/js/customizer-preview.js', array( 'customize-preview', 'jquery' ), rand(), true );
 }
 add_action( 'customize_preview_init', 'customize_preview_post_meta' );
 
@@ -176,6 +245,147 @@ if ( class_exists( 'WP_Customize_Control' ) ) {
 		<?php
 		}
 	}
+
+
+	class DSS_Toggle extends WP_Customize_Control {
+		public $type = 'toggle';
+
+		/**
+		 * Enqueue scripts/styles for the color picker.
+		 *
+		 * @since 3.4.0
+		 */
+		public function enqueue() {
+			global $_wp_admin_css_colors;
+			wp_enqueue_script( 'pure-css-toggle-buttons', get_stylesheet_directory_uri() . '/js/customizer.js', array( 'jquery' ), rand(), true );
+			wp_enqueue_style( 'pure-css-toggle-buttons', get_stylesheet_directory_uri() . '/pure-css-toggle-buttons/pure-css-togle-buttons.css', array(), rand() );
+
+			$color_scheme = array();
+			$colors = array();
+			$default_colors = array('#222','#333');
+			$user_color_scheme_name = get_user_option( 'admin_color' );
+			if ( isset( $user_color_scheme_name ) && '' !== $user_color_scheme_name ) {
+				$color_scheme = $_wp_admin_css_colors[get_user_option( 'admin_color' )];
+				$colors =  wp_parse_args( $_wp_admin_css_colors['colors'], $default_colors );
+			} else {
+				$colors = $default_colors;
+			}
+
+			/*
+			[blue] => stdClass Object
+			        (
+			            [name] => Blue
+			            [url] => http://customizer.dev/wp-admin/css/colors/blue/colors.min.css
+			            [colors] => Array
+			                (
+			                    [0] => #096484
+			                    [1] => #4796b3
+			                    [2] => #52accc
+			                    [3] => #74B6CE
+			                )
+
+			            [icon_colors] => Array
+			                (
+			                    [base] => #e5f8ff
+			                    [focus] => #fff
+			                    [current] => #fff
+			                )
+
+			        )
+			 */
+
+
+
+				$css = '
+					.disabled-control-title {
+						color: #a0a5aa;
+					}
+					input[type=checkbox].tgl-light:checked + .tgl-btn {
+				  		background: #0085ba;
+					}
+					input[type=checkbox].tgl-light + .tgl-btn {
+					  background: #a0a5aa;
+				  	}
+					input[type=checkbox].tgl-light + .tgl-btn:after {
+					  background: #f7f7f7;
+				  	}
+
+					input[type=checkbox].tgl-ios:checked + .tgl-btn {
+					  background: #0085ba;
+					}
+				';
+				wp_add_inline_style( 'pure-css-toggle-buttons' , $css );
+		}
+
+		public function render_content() {
+			global $_wp_admin_css_colors;
+			// @codingStandardsIgnoreStart
+			// printf( '<pre>%s</pre>', print_r( $_wp_admin_css_colors[get_user_option( 'admin_color' )], true ) );
+			// printf( '<pre>%s</pre>', print_r( $_wp_admin_css_colors, true ) );
+			// echo '<pre>' . wp_color_scheme_settings() . ' </pre>';
+			// @codingStandardsIgnoreEnd
+			/*
+			(
+			    [name] => Default
+			    [url] =>
+			    [colors] => Array
+			        (
+			            [0] => #222
+			            [1] => #333
+			            [2] => #0073aa
+			            [3] => #00a0d2
+			        )
+
+			    [icon_colors] => Array
+			        (
+			            [base] => #82878c
+			            [focus] => #00a0d2
+			            [current] => #fff
+			        )
+
+			)
+			*/
+			?>
+			<!-- <label> -->
+				<div style="display:flex;flex-direction: row;justify-content: flex-start;">
+				<span class="customize-control-title" style="flex: 2 0 0; vertical-align: middle;"><?php echo esc_html( $this->label ); ?></span>
+				<input id="cb1" type="checkbox" class="tgl tgl-light" value="<?php echo esc_attr( $this->value() ); ?>" <?php $this->link(); checked( $this->value() ); ?> />
+				<label for="cb1" class="tgl-btn"></label>
+				</div>
+				<?php if ( ! empty( $this->description ) ) : ?>
+				<span class="description customize-control-description"><?php echo $this->description; ?></span>
+				<?php endif; ?>
+			<!-- </label> -->
+			<?php
+			/*
+			switch ( $this->type ) {
+				case 'value':
+					# code...
+					break;
+
+				default:
+					# code...
+					break;
+			}
+
+			<input id="cb1" type="checkbox" class="tgl tgl-light"/>
+			<label for="cb1" class="tgl-btn"></label>
+
+			<input id="cb2" type="checkbox" class="tgl tgl-ios"/>
+			<label for="cb2" class="tgl-btn"></label>
+
+			<input id="cb3" type="checkbox" class="tgl tgl-skewed"/>
+		    <label data-tg-off="OFF" data-tg-on="ON" for="cb3" class="tgl-btn"></label>
+
+			<input id="cb4" type="checkbox" class="tgl tgl-flat"/>
+		    <label for="cb4" class="tgl-btn"></label>
+
+			<input id="cb5" type="checkbox" class="tgl tgl-flip"/>
+		    <label data-tg-off="Nope" data-tg-on="Yeah!" for="cb5" class="tgl-btn"></label>
+			<?php
+			*/
+		}
+	}
 }
 
 /**
@@ -194,9 +404,9 @@ function list_svg( $selected_value = '' ) {
 		$svgs = json_decode( $svg_data,true );
 		// TODO: add error control for `if ( null === $svgs && json_last_error() !== JSON_ERROR_NONE ) {}`
 
-		$ret =  '<select class="image-picker">';
+		$ret = '<select class="image-picker">';
 		foreach ( $svgs as $svg ) {
-			if ( '' !== $selected_value && $selected_value === sprintf( '%s/%s', get_stylesheet_directory_uri(), $svg['file'] ) ) {
+			if ( '' !== $selected_value && sprintf( '%s/%s', get_stylesheet_directory_uri(), $svg['file'] ) === $selected_value ) {
 				$ret .= sprintf( '<option selected data-img-class="svg-logo"  data-img-src="%1$s/%2$s" value="%1$s/%2$s" >%2$s</option>', get_stylesheet_directory_uri(), $svg['file'] );
 			} else {
 				$ret .= sprintf( '<option data-img-class="svg-logo"  data-img-src="%1$s/%2$s" value="%1$s/%2$s" >%2$s</option>', get_stylesheet_directory_uri(), $svg['file'] );
@@ -245,13 +455,20 @@ add_action( 'customize_controls_enqueue_scripts', 'customizer_svg_enqueue_script
  * @version 1.1.0
  * @return  string  custom logo, linked to home.
  */
-function add_svg_logo() {
-	$svg_logo_url  = get_theme_mod( 'svg_logo_url', '' );
-	$svg_logo_width = get_theme_mod( 'svg_logo_width', '240' );
-	$html = sprintf( '<a href="%1$s" class="custom-logo-link" rel="home" itemprop="url">%2$s</a>',
-	    esc_url( home_url( '/' ) ),
-		sprintf( '<img src="%s" class="custom-logo svg-logo" style="width: %spx"/>',$svg_logo_url, $svg_logo_width )
-	);
+function add_svg_logo( $html ) {
+	$svg_logo_remove = get_theme_mod( 'svg_logo_remove', false );
+	if ( false !== $svg_logo_remove ) {
+		$svg_logo_url  = get_theme_mod( 'svg_logo_url', '' );
+		$svg_logo_width = get_theme_mod( 'svg_logo_width', '240' );
+		if ( '' !== $svg_logo_url ) {
+			$html = sprintf( '<a href="%1$s" class="custom-logo-link" rel="home" itemprop="url">%2$s</a>',
+			    esc_url( home_url( '/' ) ),
+				sprintf( '<img src="%s" class="custom-logo svg-logo" style="width: %spx"/>',$svg_logo_url, $svg_logo_width )
+			);
+		}
+	} else {
+		remove_theme_mod( 'svg_logo_url' );
+	}
 	return $html;
 }
 add_filter( 'get_custom_logo', 'add_svg_logo' );
